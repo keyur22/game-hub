5c01db78b285c3d7221e0bb8f45f18f023a64c8d
Keyur Chaudhari <keyurchaudhari4@gmail.com>
2025-03-13 12:40:33 +0530

    Added color mode toggle

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import { Provider } from '@/components/ui/provider';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { ColorModeProvider } from './components/ui/color-mode.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider>
-      <App />
-    </Provider>
+    <ChakraProvider value={defaultSystem}>
+      <ColorModeProvider>
+        <App />
+      </ColorModeProvider>
+    </ChakraProvider>
   </StrictMode>
 );
