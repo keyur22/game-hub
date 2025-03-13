6872d5d8b4f01407b3757ebabaa38acb306894d9
Keyur Chaudhari <keyurchaudhari4@gmail.com>
2025-03-13 16:29:10 +0530

    Updated genre list component

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,34 @@
 import useGenres from '@/hooks/useGenres';
+import { getCroppedImageUrl } from '@/services/image-url';
+import { List, HStack, Image, Text, Spinner } from '@chakra-ui/react';
 
 const GenreList = () => {
   const { loading, data: genres, error } = useGenres();
 
+  if (loading)
+    return (
+      <HStack justifyContent='center' p={5}>
+        <Spinner />
+      </HStack>
+    );
+
+  if (error) return null;
+
   return (
-    <div>
+    <List.Root listStyle='none' p={5} gap={5}>
       {genres.map((genre) => (
-        <div key={genre.id}>{genre.name}</div>
+        <List.Item>
+          <HStack>
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              boxSize='32px'
+              borderRadius={8}
+            />
+            <Text ml={2}>{genre.name}</Text>
+          </HStack>
+        </List.Item>
       ))}
-    </div>
+    </List.Root>
   );
 };
 
